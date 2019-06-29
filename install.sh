ef490022e4c1d4dbab35526d6ba7df9cf5c35c26
TheGamble <TheGamble@users.noreply.github.com>
2019-06-29 15:04:06 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#get the essentials
+sudo apt update
+sudo apt -y upgrade
+sudo apt install -y git zsh curl
+
+#git and link dotfiles
+cd ~
+git clone https://github.com/TheGamble/dotfiles
+ln -s .dotfiles/.nanorc .nanorc
+
+#oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+rm .zshrc
+ln -s .dotfiles/.zshrc .zshrc
+
+#change shell
+sudo chsh -s $(which zsh)) $USER
