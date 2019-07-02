091dd41f900919297da82d92a6e820fcb87e4298
TheGamble <TheGamble@users.noreply.github.com>
2019-07-02 10:08:38 -0400

    Update landscape-config.sh

--- landscape/landscape-config.sh
+++ landscape/landscape-config.sh
@@ -3,4 +3,10 @@
 #Ensure system up to date
 sudo apt update
 sudo apt upgrade -y
+#Install client
 sudo apt install landscape-client
+#Copy and use server public key
+sudo cp ~/.dotfiles/landscape/landscape_server.pem /etc/landscape/server.pem
+echo ssl_public_key = /etc/landscape/server.pem | sudo tee -a /etc/landscape/client.conf
+#Register machine
+sudo landscape-config --account-name standalone --url https://landscape/message-system --ping-url http://landscape/ping
